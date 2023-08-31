680bdec5599b6deea67044e80d13f02c47d07bbc
Tanggal Abang <tanggalabang26@gmail.com>
2023-09-01 02:47:11 +0700

    video ke 3 cara fajar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
-        return response()->json($posts);
+        // return response()->json(['data' => $posts]);
+        return PostResource::collection($posts);
     }
 }
