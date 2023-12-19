85f8c71adb21ab8f1f6c23abbdaa8b565ed9b90a
Tanggal Abang <tanggalabang26@gmail.com>
2023-12-19 11:11:09 +0700

    hh

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,18 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'username' => 'test',
+            'password' => Hash::make('test'),
+            'name' => 'test',
+            'token' => 'test'
+        ]);
+
+        User::create([
+            'username' => 'test2',
+            'password' => Hash::make('test2'),
+            'name' => 'test2',
+            'token' => 'test2'
+        ]);
     }
 }
